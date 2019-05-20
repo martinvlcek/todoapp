698fbc41339ee50afe9b48816ca6331ef8fc4b61
Martin Vlček <martin.vlcek93@gmail.com>
2019-05-20 22:31:03 +0200

    Add current task date

--- js/app.js
+++ js/app.js
@@ -3,12 +3,35 @@
 var todoTitle = document.querySelector('.title-input');
 var todoList = document.querySelector('.todo-list');
 
+function formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var mins = date.getMinutes();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year + ', ' + hours + ':' + mins;
+}
+
 var createNewTask = function(task) {
     var listItem = document.createElement('li');
-    var textItem = document.createElement('span');
+    var textItem = document.createElement('div');
+    var dateInfo = document.createElement('span');
+    
 
+    dateInfo.innerText = formatDate(new Date());
+    dateInfo.className = 'task-date';
+    textItem.className = 'list-content';
     textItem.innerText = task;
     listItem.appendChild(textItem);
+    listItem.appendChild(dateInfo);
 
     return listItem;
 };
@@ -16,7 +39,7 @@ var createNewTask = function(task) {
 var addNewTask = function() {
     var listItem = createNewTask(todoTitle.value);
     todoList.appendChild(listItem);
-    listItem.classList.add('animacia');
+    // listItem.classList.add('animacia');
     todoTitle.value = '';
 }
 
